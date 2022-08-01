dba50d00c86516d78a196cb99c398ac2969999a3
Ivan Mermelstein <Ivan.Mermelstein@pronto.com.uy>
2022-08-01 12:50:42 -0300

    Project cleanup and prettier.config added

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    bracketSpacing: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'avoid',
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    jsxSingleQuote: true,
+    bracketSameLine: true,
+    endOfLine: 'auto',
+    printWidth: 80
+};
