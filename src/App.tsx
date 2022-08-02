04baa9ca826012bd1686dc2c428f6923752d58fe
Ivan Mermelstein <Ivan.Mermelstein@pronto.com.uy>
2022-08-02 09:48:14 -0300

    Story components mock and minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './components/Header/Header';
 import SelectorButtons from './components/SelectorButtons/SelectorButtons';
+import StoryList from './components/StoryList/StoryList';
 
 const App = () => {
     return (
         <>
             <Header />
             <SelectorButtons />
+            <StoryList />
         </>
     );
 };
